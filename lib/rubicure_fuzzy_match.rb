3cdefcd282378889ad7b035f9bd0a206a7fda530
ka <ka.kaosf@gmail.com>
2015-08-22 15:12:41 +0900

    Implement Rubicure::Series.regularize

--- lib/rubicure_fuzzy_match.rb
+++ lib/rubicure_fuzzy_match.rb
@@ -1,5 +1,24 @@
 require "rubicure_fuzzy_match/version"
+require "rubicure"
+require "fuzzy_match"
 
-module RubicureFuzzyMatch
-  # Your code goes here...
+class Rubicure::Seriese
+  TITLES = Precure.map(&:title)
+  TITLES_DICTIONARY = Hash[*TITLES.map.with_index { |e, i| [e, i] }.flatten]
+  FUZZY_TITLES_DICTIONARY = {
+    '初代'                           => 0,
+    '無印'                           => 0,
+    'ふたりはプリキュア Splash☆Star' => 2,
+    '555'                            => 4,
+    'ドドリア'                       => 9,
+    'ハチャプリ'                     => 10,
+    'ゴプリプリ'                     => 11,
+    '姫プリ'                         => 11,
+  }
+  FUZZY_MATCHER = FuzzyMatch.new(TITLES + FUZZY_TITLES_DICTIONARY.keys)
+  TITLES_DICTIONARY.merge! FUZZY_TITLES_DICTIONARY
+
+  def self.regularize(title)
+    TITLES[TITLES_DICTIONARY[FUZZY_MATCHER.find title]]
+  end
 end
