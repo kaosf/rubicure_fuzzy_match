1a28622b329c6592d592d89c2ea8d223677a1dc5
ka <ka.kaosf@gmail.com>
2015-08-23 20:52:20 +0900

    Tweak dictionary code

--- lib/rubicure_fuzzy_match.rb
+++ lib/rubicure_fuzzy_match.rb
@@ -6,21 +6,21 @@ class Rubicure::Seriese
   TITLES = Precure.map(&:title)
   TITLES_DICTIONARY = Hash[*TITLES.map.with_index { |e, i| [e, i] }.flatten]
   FUZZY_TITLES_DICTIONARY = {
-    '初代'                           => 0,
-    '無印'                           => 0,
-    'マックスハート'                 => 1,
-    'スプラッシュスター'             => 2,
-    'イエス'                         => 3,
-    '555'                            => 4,
-    'フレプリ'                       => 5,
-    'ハト'                           => 6,
-    'スイプリ'                       => 7,
-    'スマプリ'                       => 8,
-    'ドキプリ'                       => 9,
-    'ドドリア'                       => 9,
-    'ハチャプリ'                     => 10,
-    'ゴプリプリ'                     => 11,
-    '姫プリ'                         => 11,
+    '初代'               => 0,
+    '無印'               => 0,
+    'マックスハート'     => 1,
+    'スプラッシュスター' => 2,
+    'イエス'             => 3,
+    '555'                => 4,
+    'フレプリ'           => 5,
+    'ハト'               => 6,
+    'スイプリ'           => 7,
+    'スマプリ'           => 8,
+    'ドキプリ'           => 9,
+    'ドドリア'           => 9,
+    'ハチャプリ'         => 10,
+    'ゴプリプリ'         => 11,
+    '姫プリ'             => 11,
   }
   FUZZY_MATCHER = FuzzyMatch.new(TITLES + FUZZY_TITLES_DICTIONARY.keys)
   TITLES_DICTIONARY.merge! FUZZY_TITLES_DICTIONARY
