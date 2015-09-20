01457d2d198fac18187bbbff21f91ee6155ee8ae
ka <ka.kaosf@gmail.com>
2015-09-20 17:44:08 +0900

    Remove unnecessray spaces of .fuzzy_find test data

--- test/test_rubicure_fuzzy_match.rb
+++ test/test_rubicure_fuzzy_match.rb
@@ -43,18 +43,18 @@ class TestRubicureFuzzyMatch < Test::Unit::TestCase
   end
 
   data(
-    "ふたり"      => ["ふたり"     , Precure.first           ],
-    "MH"          => ["MH"         , Precure.max_heart       ],
-    "SS"          => ["SS"         , Precure.splash_star     ],
-    "Yes"         => ["Yes"        , Precure.yes             ],
-    "5GoGo"       => ["5GoGo"      , Precure.yes_gogo        ],
-    "フレ"        => ["フレ"       , Precure.fresh           ],
-    "ハト"        => ["ハト"       , Precure.heart_catch     ],
-    "スイ"        => ["スイ"       , Precure.suite           ],
-    "スマ"        => ["スマ"       , Precure.smile           ],
-    "ドキ"        => ["ドキ"       , Precure.dokidoki        ],
-    "ハピチャ"    => ["ハピチャ"   , Precure.happiness_charge],
-    "Goプリ"      => ["Goプリ"     , Precure.go_princess     ])
+    "ふたり"   => ["ふたり"  , Precure.first           ],
+    "MH"       => ["MH"      , Precure.max_heart       ],
+    "SS"       => ["SS"      , Precure.splash_star     ],
+    "Yes"      => ["Yes"     , Precure.yes             ],
+    "5GoGo"    => ["5GoGo"   , Precure.yes_gogo        ],
+    "フレ"     => ["フレ"    , Precure.fresh           ],
+    "ハト"     => ["ハト"    , Precure.heart_catch     ],
+    "スイ"     => ["スイ"    , Precure.suite           ],
+    "スマ"     => ["スマ"    , Precure.smile           ],
+    "ドキ"     => ["ドキ"    , Precure.dokidoki        ],
+    "ハピチャ" => ["ハピチャ", Precure.happiness_charge],
+    "Goプリ"   => ["Goプリ"  , Precure.go_princess     ])
   test '.fuzzy_find' do |(input, expected)|
     assert_equal expected, Rubicure::Seriese.fuzzy_find(input)
   end
