2f49a5bb04622b70a64f138b11b27031d089b841
ka <ka.kaosf@gmail.com>
2016-12-31 05:06:27 +0900

    Revert "Update add_filter argument"
    This reverts commit f75076a7848b40ef9091f60427fc3a2b9927ed70.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ Coveralls.wear!
 if ENV['CI'] != 'true'
   SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
   SimpleCov.start do
-    add_filter 'test/test_'
+    add_filter '/test/'
   end
 end
 
@@ -15,5 +15,5 @@ end
 # ENV['CI'] != 'true" ref.
 #   http://stackoverflow.com/questions/12983137/how-do-detect-if-travis-ci-or-not
 #   http://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
-# add_filter ref.
+# add_filter '/test/' ref.
 #   https://github.com/colszowka/simplecov#string-filter
